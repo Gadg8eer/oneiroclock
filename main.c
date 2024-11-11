e9812fa74d623357563fe991b585beaf4d24ec2b
Gadg8eer <64449092+Gadg8eer@users.noreply.github.com>
2024-11-11 11:21:39 -0700

    Update main.c

--- main.c
+++ main.c
@@ -6,10 +6,11 @@ int main(void) {
 	// replace 7200 with your own offset
 	// UTC+2 is 3600*2 = 7200
 	// thats how you change the timezone of this clock
-	int hour = ((CURRENT_TIME+7200)%86400)/3600;
+	int hour = ((CURRENT_TIME+0000)%86400)/3600;
 	int min = (CURRENT_TIME % 3600) / 60;
+	int sec = (CURRENT_TIME % 60) / 60;
 	// swatch beat
 	int beat = ((CURRENT_TIME + 3600) % 86400 ) / 86.4;
-	printf("%02d:%02d @%03d\n", hour, min, beat);
+	printf("%02d:%02d:%02d @%03d\n", hour, min, sec, beat);
 	return 0;
 }
